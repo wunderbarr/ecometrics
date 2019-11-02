bafa613e05e0da9e6a25f72422c3f60c52ef5ee4
wunderbarr <wunderbardan@gmail.com>
2019-11-02 12:20:14 +0100

    add s and r_square metrics

--- code/TrainExer13.py
+++ code/TrainExer13.py
@@ -1,3 +1,18 @@
 import pandas as pd
-data = pd.read_excel('TrainExer13.xls')
+import numpy as np
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error as mse
+data = pd.read_excel('../dataset/TrainExer13.xls')
 print (data.head())
+W = data['Winning time men']
+G = np.array(data['Game']).reshape((-1, 1))
+model = LinearRegression()
+model.fit(G, W)
+r_sq = model.score(G, W)
+print('intercept a:', model.intercept_)
+print('slope b:', model.coef_)
+print('coefficient of determination r_sq:', r_sq)
+print ('s:', np.sqrt(np.sum(np.square(np.subtract(W, model.predict(G))))/(len(G)-2)) )
+# print ('s:', np.sqrt(np.sum(np.subtract(W, model.predict(G)))/(n-2)))
+y_pred = model.predict(np.array([16, 17, 18]).reshape((-1, 1)))
+print('predicted response:', y_pred, sep='\n')
